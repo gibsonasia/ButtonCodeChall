aee9e3f2d4a4435518fd03132bc70ca8d133824d
gibsonasia <gibsonjasia@gmail.com>
2018-03-07 14:22:20 -0500

    create user class created and xml files have been updated

--- app/src/main/java/com/gibson/a/fakebutton/view/CreateNewUserActivity.java
+++ app/src/main/java/com/gibson/a/fakebutton/view/CreateNewUserActivity.java
@@ -1,14 +1,20 @@
 package com.gibson.a.fakebutton.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.gibson.a.fakebutton.R;
 
 public class CreateNewUserActivity extends AppCompatActivity {
 
-    EditText name,email,candidate;
+   public EditText name,email,candidate;
+    Button addClientBttn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +24,28 @@ public class CreateNewUserActivity extends AppCompatActivity {
         name = findViewById(R.id.name_et);
         email = findViewById(R.id.email_et);
         candidate = findViewById(R.id.canditate_et);
+        addClientBttn = findViewById(R.id.add_client_bttn);
+
+        addClientBttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               if(TextUtils.isEmpty(name.getText()) || TextUtils.isEmpty(name.getText())||
+                       TextUtils.isEmpty(email.getText())){
+
+                   Toast.makeText(getApplicationContext(), "Check all feilds, try again", Toast.LENGTH_LONG).show();
+
+               }
+
+
+            }
+        });
+
+
+    }
+
+    public void sendUserData(View view){
+
+        Intent intent = new Intent();
+        String name_field = name.getText().toString();
     }
 }
